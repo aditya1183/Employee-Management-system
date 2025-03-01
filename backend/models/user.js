155b80b4090f4947a3868744c093f0fb51d36021
aditya1183 <adityasharma44159@gmail.com>
2025-03-01 23:37:33 +0530

    edit

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-const mongoValidator = require("mongoose-unique-validator");
+// const mongoValidator = require("mongoose-unique-validator");
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minLength: 6 },
-  joiningDate: { type: Date, required:  true },
+  joiningDate: { type: Date, required: true },
   position: { type: String, required: true },
   name: { type: String, required: true },
   aadhar: { type: String, required: true },
@@ -21,12 +21,12 @@ const userSchema = new mongoose.Schema({
   image: { type: String },
   address: { type: String, required: true },
   linkedInId: { type: String, required: true },
-  phone: { type: String, required: true },
+  phone: { type: String },
   githubId: { type: String, required: true },
   dateOfBirth: { type: Date, required: true },
 });
 
-userSchema.plugin(mongoValidator);
+// userSchema.plugin(mongoValidator);
 
 const userModel = mongoose.model("User", userSchema);
 
